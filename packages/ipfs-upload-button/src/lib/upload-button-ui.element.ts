b45da08fdbeba8e38a1919153d1cb3ce0454580d
FazioNico <contact@nicolasfazio.ch>
2022-10-03 10:45:18 +0200

    refactor(ui): add css rules

--- packages/ipfs-upload-button/src/lib/upload-button-ui.element.ts
+++ packages/ipfs-upload-button/src/lib/upload-button-ui.element.ts
@@ -65,17 +65,84 @@ export abstract class UploadButtonUI extends HTMLElement {
           #storage-result {
             display: none;
           }
+          #storage-result a {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            align-items: center;
+            align-content: center;
+          }
+          #storage-result a img {
+            margin-right: 10px;
+          }
           .spinner {
-            width: 14px;
-            height: 14px;
-            vertical-align: middle;
             display: none;
           }
+          .lds-ellipsis {
+            display: inline-block;
+            position: relative;
+            width: 80px;
+            height: 80px;
+          }
+          .lds-ellipsis div {
+            position: absolute;
+            top: 33px;
+            width: 13px;
+            height: 13px;
+            border-radius: 50%;
+            background: #000;
+            animation-timing-function: cubic-bezier(0, 1, 1, 0);
+          }
+          .lds-ellipsis div:nth-child(1) {
+            left: 8px;
+            animation: lds-ellipsis1 0.6s infinite;
+          }
+          .lds-ellipsis div:nth-child(2) {
+            left: 8px;
+            animation: lds-ellipsis2 0.6s infinite;
+          }
+          .lds-ellipsis div:nth-child(3) {
+            left: 32px;
+            animation: lds-ellipsis2 0.6s infinite;
+          }
+          .lds-ellipsis div:nth-child(4) {
+            left: 56px;
+            animation: lds-ellipsis3 0.6s infinite;
+          }
+          @keyframes lds-ellipsis1 {
+            0% {
+              transform: scale(0);
+            }
+            100% {
+              transform: scale(1);
+            }
+          }
+          @keyframes lds-ellipsis3 {
+            0% {
+              transform: scale(1);
+            }
+            100% {
+              transform: scale(0);
+            }
+          }
+          @keyframes lds-ellipsis2 {
+            0% {
+              transform: translate(0, 0);
+            }
+            100% {
+              transform: translate(24px, 0);
+            }
+          }          
       </style> 
       <button part="btn">
           <slot>upload</slot>
       </button>
-      <span part="spinner" class="spinner"></span>
+
+      <div part="spinner" class="spinner">
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+      </div>
+
       <div part="result" id="storage-result"></div>
       <input type="file" multiple style="display: none;" />
     `;
@@ -165,6 +232,7 @@ export abstract class UploadButtonUI extends HTMLElement {
           return `
             <a href="${ipfsUrl}">
               <img src="${localUrl}" width="100" />
+              ${ipfsUrl}
             </a>
           `;
         })
